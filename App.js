19fa9d2c42c82ed491ee675115f7a1c308fc2ace
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-14 03:22:51 -0800

    Exercise 10.2

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    var count = "Hello"; // Use of var instead of let/const
+    for(var i = 0; i < 5; i++){ // Incorrect spacing, use of var
+        console.log('Logging to the console:', count);
+    }
+    return (
+        <View style={styles.container}>
+            <Text>hi Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    unusedStyle: { // Unused style that should trigger no-unused-vars
+        borderWidth: 1,
+    }
 });
