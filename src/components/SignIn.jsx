0ab6a3f2c0a356ea3490d58f8bdb8590ad75e5e5
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-15 06:49:10 -0800

    submit values

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,45 @@
-import Text from './Text';
+import React from 'react';
+import { View, Pressable, Text } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme'; // make sure to import your theme
 
+// Your SignIn component
 const SignIn = () => {
-    return <Text>The sign-in view</Text>;
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
+    return (
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            onSubmit={onSubmit}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <View>
+                    <FormikTextInput
+                        name="username"
+                        placeholder="Username"
+                        onChangeText={handleChange('username')}
+                        onBlur={handleBlur('username')}
+                        value={values.username}
+                    />
+                    <FormikTextInput
+                        name="password"
+                        placeholder="Password"
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        value={values.password}
+                        secureTextEntry={true}
+                    />
+                    {/* When this is pressed, Formik's handleSubmit will call the onSubmit function with form values */}
+                    <Pressable onPress={handleSubmit}>
+                        <Text>Sign In</Text>
+                    </Pressable>
+                </View>
+            )}
+        </Formik>
+    );
 };
 
 export default SignIn;
