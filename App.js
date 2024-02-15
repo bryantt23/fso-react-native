3362fe569adfa5cd3f51fddcde57f3998472ff22
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-15 05:01:32 -0800

    add component

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>hi Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+const App = () => {
+    return <Main />;
+};
 
-const styles = StyleSheet.create({
-    alignItems: 'center',
-    justifyContent: 'center',
-});
+export default App;
