406e7dda1009e64b5ef34eafd6fe25e338e7bf73
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-17 00:11:11 -0800

    custom hook

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,6 @@
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem'
-import { useState, useEffect } from 'react';
+import useRepositories from '../../hooks/useRepositories'
 
 const styles = StyleSheet.create({
     separator: {
@@ -11,21 +11,7 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-    const [repositories, setRepositories] = useState();
-    console.log('RepositoryList')
-    const fetchRepositories = async () => {
-        // Replace the IP address part with your own IP address!
-        const response = await fetch('http://192.168.1.64:5000/api/repositories');
-        const json = await response.json();
-
-        console.log(json);
-
-        setRepositories(json);
-    };
-
-    useEffect(() => {
-        fetchRepositories();
-    }, []);
+    const { repositories } = useRepositories();
 
     // Get the nodes from the edges array
     const repositoryNodes = repositories
