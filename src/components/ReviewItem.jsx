653bd3d5380a307c963af86965b631992454301f
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-17 07:13:46 -0800

    style

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -3,41 +3,65 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const reviewStyles = StyleSheet.create({
     container: {
+        backgroundColor: '#ffffff',
         padding: 10,
-        backgroundColor: '#f0f0f0',
+        marginTop: 10,
         borderRadius: 5,
-        marginVertical: 5,
-    },
-    text: {
-        marginBottom: 5,
+        borderWidth: 1,
+        borderColor: '#e1e4e8',
     },
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginBottom: 5,
+        marginBottom: 10,
     },
     rating: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        borderColor: '#0366d6',
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    ratingText: {
+        color: '#0366d6',
         fontWeight: 'bold',
     },
+    username: {
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
     date: {
-        color: '#787878',
+        color: '#586069',
+    },
+    reviewText: {
+        color: '#24292e',
     },
 });
 
-const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-};
-
 const ReviewItem = ({ review }) => {
+    // Format date to a more human-readable form
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+        const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1; // JS months are 0-indexed
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    };
+
     return (
         <View style={reviewStyles.container}>
             <View style={reviewStyles.header}>
-                <Text style={reviewStyles.rating}>Rating: {review.rating}</Text>
-                <Text style={reviewStyles.date}>{formatDate(review.createdAt)}</Text>
+                <View style={reviewStyles.rating}>
+                    <Text style={reviewStyles.ratingText}>{review.rating}</Text>
+                </View>
+                <View>
+                    <Text style={reviewStyles.date}>{formatDate(review.createdAt)}</Text>
+                    <Text style={reviewStyles.username}>{review.user.username}</Text>
+                </View>
             </View>
-            <Text style={reviewStyles.text}>{review.text}</Text>
-            <Text>Reviewer: {review.user.username}</Text>
+            <Text style={reviewStyles.reviewText}>{review.text}</Text>
         </View>
     );
 };
